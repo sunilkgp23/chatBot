02a95a0ba4a23ac1f91899d1c73af1ed041e24ea
Your Name <you@example.com>
2017-12-18 01:15:34 +0530

    Chat Bot Working Code

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Please Open the chatBox.html in any Browser to host from file system Or Run the same file from Any Server having all the folder content.
+
+Currently it is using a service from different server, so please install the chrome plug in "Allow-Control-Allow-Origin: *"
---
+++ chatBox.js
@@ -0,0 +1,53 @@
+var count=0;
+var botURL="";
+
+function sendMsg() {
+	var msgInput=$("#inputMsg").val();
+	$("#inputMsg").val("");
+	if(msgInput!=""){
+		count++;
+		$('#displayMsg').append(
+			$('<div/>').attr("id", "newDiv" + count)
+			.addClass("userBox")
+			.append("<p/>")
+			.text(msgInput)
+		);
+		$("#displayMsg").scrollTop(function() { return this.scrollHeight; });
+		var chatContent = document.getElementById("displayMsg").innerHTML;
+		localStorage.setItem("chatContent", chatContent);
+		botURL="http://www.personalityforge.com/api/chat/?apiKey=6nt5d1nJHkqbkphe&message="+msgInput+"&chatBotID=63906&externalID=chirag1";
+		
+		$.ajax({
+			url: botURL,
+			method: 'GET',
+			async: true,
+			dataType: "json"
+		}).then(function (botData) {
+			console.log(botData.message.message);
+			count++;
+			$('#displayMsg').append(
+				$('<div/>').attr("id", "newDiv" + count)
+				.addClass("botBox")
+				.append("<p/>")
+				.text(botData.message.message)
+			);
+			$("#displayMsg").scrollTop(function() { return this.scrollHeight; });
+			var chatContent = document.getElementById("displayMsg").innerHTML;
+			localStorage.setItem("chatContent", chatContent);
+		});
+	}	
+}
+ 
+$(window).load(function () {
+	 document.getElementById("displayMsg").innerHTML = localStorage.getItem("chatContent");
+	 $('#inputMsg').keypress(function (e) {
+	 var key = e.which;
+	 if(key == 13) 
+	  {
+		sendMsg();
+		 $(this).val('').focus(); 
+		 return false;
+	  }
+	});
+});
+
